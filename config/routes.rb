57db867b73bc35263c043ce39258163575a6e08d
Babin Bohora <bohorababin1@gmail.com>
2022-01-08 12:34:45 +0900

    basic index page done

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,16 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+
+  # list all tasks
+  get 'tasks', to: "tasks#index"
+  # view a task
+  get 'tasks/:id', to: "tasks#show", as: "task"
+  # add a task
+  get 'tasks/new', to: "tasks#new", as: "new_task"
+  post 'tasks', to: "tasks#create"
+  # edit a task
+  get 'tasks/:id/edit', to: "tasks#edit", as: "edit_task"
+  patch 'tasks/:id', to: "tasks#update"
+  # destroy all task
+  delete 'tasks/:id', to: "tasks#destroy"
 end
