32c2c1a81e867781949df0ccaee3412734ffdb3b
Babin Bohora <bohorababin1@gmail.com>
2022-01-08 14:40:09 +0900

    edit and new done

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -4,27 +4,41 @@ class TasksController < ApplicationController
   end
   
   def show
-  
+    @task = Task.find(params[:id])
   end
-
+  
   def new
-  
+    @task = Task.new
   end
-
+  
   def create
-  
+    # raise
+    task = Task.create(param_filter)
+    redirect_to task_path(task)
   end
-
-  def edit
   
+  def edit
+    @task = Task.find(params[:id])
   end
 
   def update
-  
+    @task = Task.find(params[:id])
+    @task.update(param_filter)
   end
 
   def destroy
-  
+    @task = Task.find(params[:id])
+    @task.destroy
   end
+
+  private
+
+  def param_filter
+    params.require(:task).permit(:title, :details)
+  end
+
+  # def finding_id
+  #   @task = Task.find(params[:id])
+  # end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,11 +3,12 @@ Rails.application.routes.draw do
 
   # list all tasks
   get 'tasks', to: "tasks#index"
-  # view a task
-  get 'tasks/:id', to: "tasks#show", as: "task"
+
   # add a task
   get 'tasks/new', to: "tasks#new", as: "new_task"
   post 'tasks', to: "tasks#create"
+  # view a task
+  get 'tasks/:id', to: "tasks#show", as: "task"
   # edit a task
   get 'tasks/:id/edit', to: "tasks#edit", as: "edit_task"
   patch 'tasks/:id', to: "tasks#update"
